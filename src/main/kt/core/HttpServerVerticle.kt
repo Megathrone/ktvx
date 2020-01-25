0263a276be8499c8d4a36a4afe12b2ece137e059
Megathrone <yizhouwang418@gmail.com>
2020-01-24 23:44:59 -0500

    lambda expression

--- src/main/kt/core/HttpServerVerticle.kt
+++ src/main/kt/core/HttpServerVerticle.kt
@@ -1,18 +1,23 @@
 package core
 
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.http.HttpServer
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
 class HttpServerVerticle : AbstractVerticle() {
 
-
     override fun start() {
-        val server:HttpServer = vertx.createHttpServer();
-        server.requestHandler { httpServerRequest ->
-            var resp = httpServerRequest.response()
-            resp.putHeader("Content-type", "text/plain; charset=utf-8")
-            resp.end("Hello from vertx with kotlin")
-        }
-        server.listen(9999)
+        val server = vertx.createHttpServer();
+        val router = Router.router(vertx)
+
+        router.route().path("/:name").handler(make)
+        server.requestHandler(router).listen(9999)
+    }
+
+    val make: (RoutingContext) -> Unit = { context ->
+        var resp = context.response()
+        var name = context.request().getParam("name")
+        resp.putHeader("Content-type", "text/plain; charset=utf-8")
+        resp.end("Hello from vertx with kotlin: $name")
     }
 }
